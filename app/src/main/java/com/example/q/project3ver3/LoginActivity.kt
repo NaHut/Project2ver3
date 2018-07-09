561701d7854e8005c1c3d14fc04817b6fef583ed
NaHut <whow1111@naver.com>
2018-07-09 13:52:10 +0900

    Implement SignIn and SignUp

--- app/src/main/java/com/example/q/project3ver3/LoginActivity.kt
+++ app/src/main/java/com/example/q/project3ver3/LoginActivity.kt
@@ -2,25 +2,31 @@ package com.example.q.project3ver3
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
-import com.android.volley.AuthFailureError
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
+import android.widget.Toast
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import org.json.JSONObject
-import com.android.volley.VolleyError
-import com.android.volley.RequestQueue
-import java.util.HashMap
+import java.util.*
 
+const val LOGIN = 0
+const val WRONG_PW = 1
+const val NOT_MEMBER = 2
+const val ALREADY_MEMBER = 3
+const val SIGN_UP = 4
 
+var serverRes = NOT_MEMBER
 
 class LoginActivity : AppCompatActivity() {
 
     val url = "http://143.248.36.215:8080"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,44 +37,51 @@ class LoginActivity : AppCompatActivity() {
         var btnSignIn = findViewById<Button>(R.id.signIn)
         var btnSignUp = findViewById<Button>(R.id.signUp)
 
+        //Sign In 버튼
         btnSignIn.setOnClickListener {
             var userId = inputUserId.text;
             var password = inputPassword.text;
+            var result = SignIn(userId.toString(), password.toString())
 
-            //check if he is memeber or not!!
-            if(checkIsMember(userId.toString(), password.toString())){
-                //He is Memeber
+            if(result == LOGIN){
                 val myIntent = Intent(this,MainActivity::class.java)
                 startActivity(myIntent)
                 finish()
             }
-            else{
-                //He is not member
+            else if(result == WRONG_PW){
+
             }
+            else{
+
+            }
+
         }
 
         btnSignUp.setOnClickListener {
-
-            //alert dialog
+            showSignDialog()
         }
     }
 
-    fun checkIsMember (userId : String , password : String) : Boolean{
+    fun SignIn (userId : String , password : String) : Int{
         val data = HashMap<String,String>()
 
         data.put("userId", userId)
         data.put("password", password)
         data.put("tag", "login")
 
-        postData(url,data)
-        return true
+        postDataForSign(url,data)
+
+        return serverRes
     }
 
-    fun postData(url: String, data: HashMap<String,String>) {
+    fun postDataForSign(url: String, data: HashMap<String,String>) {
         val requstQueue = Volley.newRequestQueue(this@LoginActivity)
+        var result : String
 
         val jsonobj = object : JsonObjectRequest(Request.Method.POST, url, JSONObject(data),
                 Response.Listener { response ->
+                    result = response.getString("result")
+                    changeServerRes(result)
                     print("success")
                 },
                 Response.ErrorListener { error ->
@@ -80,5 +93,58 @@ class LoginActivity : AppCompatActivity() {
         }
         requstQueue.add(jsonobj)
     }
+    fun showSignDialog() {
+        val layoutInflater = LayoutInflater.from(this@LoginActivity)
+        val view = layoutInflater.inflate(R.layout.sign_dialog,null)
+
+        val alertDialogBuilderUserInput = AlertDialog.Builder(this@LoginActivity)
+        alertDialogBuilderUserInput.setView(view)
+
+        val inputId = view.findViewById<EditText>(R.id.signId)
+        val inputPW = view.findViewById<EditText>(R.id.signPW)
+
+        alertDialogBuilderUserInput.setPositiveButton("Register"){
+            dialog, which->
+            val result = signUp(inputId.text.toString(),inputPW.text.toString())
+            if(result == SIGN_UP){
+                Toast.makeText(applicationContext, "Register OK", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(applicationContext, "ID is existed", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        alertDialogBuilderUserInput.setNeutralButton("Cancel"){_,_->
+            Toast.makeText(applicationContext,"Canceled", Toast.LENGTH_SHORT).show()
+        }
+
+        val dialog: AlertDialog = alertDialogBuilderUserInput.create()
+
+        dialog.show()
+    }
+
+    fun signUp(userId: String, password: String) : Int {
+        val data = HashMap<String,String>()
+
+        data.put("userId", userId)
+        data.put("password", password)
+        data.put("tag", "signUp")
+
+        postDataForSign(url,data)
+
+        return serverRes
+    }
+
+    fun changeServerRes(result : String){
+        when(result){
+            "LOGIN" -> serverRes = LOGIN
+            "WRONG_PW" -> serverRes = WRONG_PW
+            "NOT_MEMBER" -> serverRes = NOT_MEMBER
+            "ALREADY_MEMBER" -> serverRes = ALREADY_MEMBER
+            "SIGN_UP" -> serverRes = SIGN_UP
+        }
+    }
 }
+
 
